073e8beb2a0caffa43601dc806d44f2cc3f12ffc
Dan Tao <daniel.tao@gmail.com>
2014-01-22 07:16:19 -0800

    implemented base logic for converting Markdown to slides

--- slider.js
+++ slider.js
@@ -5,9 +5,64 @@ window.addEventListener('load', function() {
   request.open('GET', title + '.markdown');
 
   request.addEventListener('load', function() {
-    var html = marked(request.responseText);
-    document.body.innerHTML = html;
+    var html = marked(request.responseText),
+        doc  = parseHTML(html);
+
+    eachSlide(doc, function(slide) {
+      var element = document.createElement('DIV');
+      element.className = 'slide';
+      element.setAttribute('data-layout', slide.layout);
+      element.innerHTML = slide.html;
+      document.body.appendChild(element);
+    });
   });
 
   request.send();
+
+  function parseHTML(html) {
+    var wrapper = document.createElement('DIV');
+    wrapper.innerHTML = html;
+    return wrapper.children;
+  }
+
+  function eachSlide(doc, callback) {
+    var parts = [];
+
+    forEach(doc, function(element) {
+      if (element.tagName === 'HR') {
+        callback(createSlide(parts));
+        parts = [];
+        return;
+      }
+
+      parts.push(element);
+    });
+
+    if (parts.length > 0) {
+      callback(createSlide(parts));
+    }
+  }
+
+  function forEach(collection, callback) {
+    return Array.prototype.forEach.call(collection, callback);
+  }
+
+  function createSlide(parts) {
+    return {
+      layout: getSlideLayout(parts),
+      html: parts.map(function(part) { return part.outerHTML; }).join('')
+    };
+  }
+
+  function getSlideLayout(parts) {
+    var key = parts.map(function(part) { return part.tagName; }).join(',');
+
+    switch (key) {
+      case 'H1,H2':
+        return 'title-subtitle';
+
+      default:
+        return 'default';
+    }
+  }
 });
